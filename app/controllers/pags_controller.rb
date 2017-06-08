a13a26ab93ac28eeaaeab682eef5535a8b9592d9
Myckel <m.uribe@portal53.cl>
2017-06-07 20:39:07 -0400

    fin taller

--- app/controllers/pags_controller.rb
+++ app/controllers/pags_controller.rb
@@ -1,7 +1,16 @@
 class PagsController < ApplicationController
+  before_action :get_data
   def pag1
+    render layout: "landing"
   end
 
   def pag2
+  end
+  def pag3
+      
+  end
+  private
+  def get_data
+    @users = ["Alumno1","Alumno2", "Alumno3"]      
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'pags/pag1'
 
   get 'pags/pag2'
+  get 'pags/pag3'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
